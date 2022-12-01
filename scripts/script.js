094c1a83510b0015857c148b691098a3360c73bf
robindekker1999 <118281378+robindekker1999@users.noreply.github.com>
2022-12-01 09:42:15 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@ console.log("test");
 
 // Menu button in Nav // 
 var menuButton = document.querySelector("header nav button");
-var navMenu = document.querySelector("nav ul:nth-of-type(2)")
+var navMenu = document.querySelector("nav ul:last-of-type")
 
 menuButton.addEventListener("click", openMenu);
 
